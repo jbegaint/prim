170b9f4019e64b8637a681b9c98281c40a13e64d
Jean B <jeanbegaint@gmail.com>
2013-04-06 18:29:08 +0200

    fix arc?h

--- arc.h
+++ arc.h
@@ -1,20 +1,20 @@
-#ifndef ARCH_H
-#define ARCH_H
+#ifndef ARC_H
+#define ARC_H
 
 #include "sommet.h"
 
 /* Arc */
-// typedef struct {
-// 	int numero_debut;
-// 	int numero_fin;
-// 	float cout;
-// } Arc;
-
 typedef struct {
-	Sommet sommet_depart;
-	Sommet sommet_arrivee;
+	int numero_debut;
+	int numero_fin;
 	float cout;
 } Arc;
+
+// typedef struct {
+// 	Sommet sommet_depart;
+// 	Sommet sommet_arrive;
+// 	float cout;
+// } Arc;
 
 /* Liste d’Arc*/
 typedef struct cellule {
