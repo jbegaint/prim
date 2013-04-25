cad759c7e5ae154287fa51e3182d2dd3d0a454e0
Jean B <jean.begaint@gmail.com>
2013-04-25 19:20:03 +0200

    indent

--- src/mainview.c
+++ src/mainview.c
@@ -13,78 +13,69 @@
 
 #include "utils.h"
 
-
-
-int main( int argc, char* argv[]){
-	
-   	
-	Sommet* tab_sommet;
-	Arc* tab_arc;
+int main(int argc, char **argv)
+{
+	Sommet *tab_sommet;
+	Arc *tab_arc;
 	int len_tab_arc, len_tab_sommet;
 	File fileACM;
 	float cout_chemin;
 	int num_depart;
-	float x1,y1,x2,y2;
-	File* p;
+	float x1, y1, x2, y2;
+	File *p;
 
 
-if (argc < 2) {
+	if (argc < 2) {
 		fprintf(stderr, "Usage: %s file\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	lecture(argv[1], &tab_sommet, &tab_arc, &len_tab_sommet, &len_tab_arc);
-	
-	init_SDL ();
-	
-	SDL_Surface* ecran = NULL; /*Defini un pointeur vers l'écran*/
+	lecture(argv[1], &tab_sommet, &tab_arc, &len_tab_sommet,
+		&len_tab_arc);
 
-	ecran=init_ecran(ecran);
+	init_SDL();
 
-	ecran=edit_point(ecran, tab_sommet,len_tab_sommet);
+	SDL_Surface *ecran = NULL;	/*Defini un pointeur vers l'écran */
 
+	ecran = init_ecran(ecran);
 
+	ecran = edit_point(ecran, tab_sommet, len_tab_sommet);
 
-
-	
-
-	if (argc > 2){
+	if (argc > 2) {
 		num_depart = atoi(argv[2]);
-	}
-	else {
-		printf("Entrez le numéro du sommet de départ: [0,%d]\n", len_tab_sommet);
+	} else {
+		printf("Entrez le numéro du sommet de départ: [0,%d]\n",
+		       len_tab_sommet);
 		scanf("%d", &num_depart);
 	}
-  
-	num_depart =  get_param_sommet(num_depart, len_tab_sommet);
 
-	fileACM = algo_fileACM(tab_sommet, tab_arc, len_tab_sommet, len_tab_arc, num_depart, &cout_chemin); /*Récupère la file ACM, ainsi que le cout*/	
+	num_depart = get_param_sommet(num_depart, len_tab_sommet);
 
-		
-	ecran=affiche_cout (ecran, cout_chemin);
-	 	
-		pause_sdl(); /* Mise en pause du programme*/
+	fileACM = algo_fileACM(tab_sommet, tab_arc, len_tab_sommet, len_tab_arc, num_depart, &cout_chemin);	/*Récupère la file ACM, ainsi que le cout */
 
-	
+
+	ecran = affiche_cout(ecran, cout_chemin);
+
+	pause_sdl();		/* Mise en pause du programme */
+
+
 	/*for (p = fileACM->suiv; p != fileACM; p = (*p)->suiv)
-	{	
-		
-		x1=tab_sommet[(*(Arc*)p).sommet_depart].coordonnee_x;
-		y1=tab_sommet[(*(Arc*)p).sommet_depart].coordonnee_y;
+	   {    
 
-		x2=tab_sommet[(*(Arc*)p).sommet_arrive].coordonnee_x;
-		y2=tab_sommet[(*(Arc*)p).sommet_arrive].coordonnee_y;
-		
-		ecran = Ligne(ecran, x1, y1,x2, y2) ;
-		SDL_Flip(ecran);
-	} Seg Fault */
-	
-	pause(); /* Fonction de mise en pause du programme*/
+	   x1=tab_sommet[(*(Arc*)p).sommet_depart].coordonnee_x;
+	   y1=tab_sommet[(*(Arc*)p).sommet_depart].coordonnee_y;
 
-	SDL_Quit(); /*quitte SDL*/
-	
-	
-	return 0; 
+	   x2=tab_sommet[(*(Arc*)p).sommet_arrive].coordonnee_x;
+	   y2=tab_sommet[(*(Arc*)p).sommet_arrive].coordonnee_y;
+
+	   ecran = Ligne(ecran, x1, y1,x2, y2) ;
+	   SDL_Flip(ecran);
+	   } Seg Fault */
+
+	pause();		/* Fonction de mise en pause du programme */
+
+	SDL_Quit();		/*quitte SDL */
+
+
+	return 0;
 }
-
-
--- src/view.c
+++ src/view.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 /*#include <stdico.h>*/
-#include <time.h> 
+#include <time.h>
 #include <math.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
@@ -12,142 +12,143 @@
 #include "file.h"
 #include "utils.h"
 
-void pause_sdl() /*permet de conserver l'affichage et d'initialiser la croix pour fermer le programme*/
-{
-    int continuer = 1;
-    
+void pause_sdl()
+{				/*permet de conserver l'affichage et d'initialiser la croix pour fermer le programme */
+	int continuer = 1;
 
-    SDL_Event event;
-  
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT: 
-                continuer = 0;
-        }
-    }
+
+	SDL_Event event;
+
+	while (continuer) {
+		SDL_WaitEvent(&event);
+		switch (event.type) {
+		case SDL_QUIT:
+			continuer = 0;
+		}
+	}
 }
 
 
-void init_SDL (void)
-{ if (SDL_Init(SDL_INIT_VIDEO) == -1) /* Démarrage de la SDL. Si erreur :*/
-    {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); /* Écriture de l'erreur */
-        exit(EXIT_FAILURE); /* On quitte le programme*/
-    }
+void init_SDL(void)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) == -1) {	/* Démarrage de la SDL. Si erreur : */
+		fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());	/* Écriture de l'erreur */
+		exit(EXIT_FAILURE);	/* On quitte le programme */
+	}
 }
 
-SDL_Surface* init_ecran (SDL_Surface* ecran)
+SDL_Surface *init_ecran(SDL_Surface * ecran)
 {
-        /* ouvre une fenetre de 640*480 32 bits, dans la mémoire video ou double buffer*/
-	ecran=SDL_SetVideoMode(600, 700, 32, SDL_HWSURFACE | SDL_DOUBLEBUF); 
-	SDL_WM_SetCaption("Chemin le plus court", NULL);/* Nomme la fenetre*/
-	SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 70, 70, 70));/*Mise en couleur de l'écran*/
-	SDL_Flip(ecran); /*Mise à jour de l'écran*/
-	return ecran;	
+	/* ouvre une fenetre de 640*480 32 bits, dans la mémoire video ou double buffer */
+	ecran =
+	    SDL_SetVideoMode(600, 700, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	SDL_WM_SetCaption("Chemin le plus court", NULL);	/* Nomme la fenetre */
+	SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 70, 70, 70));	/*Mise en couleur de l'écran */
+	SDL_Flip(ecran);	/*Mise à jour de l'écran */
+	return ecran;
 }
 
 
-SDL_Surface* edit_point(SDL_Surface* ecran, Sommet* sommet, int len_tab_sommet)
+SDL_Surface *edit_point(SDL_Surface * ecran, Sommet * sommet,
+			int len_tab_sommet)
 {
-	int i=0;
+	int i = 0;
 
-	/* Sert pour les couleurs des sommets*/
-	char a,b,c;
+	/* Sert pour les couleurs des sommets */
+	char a, b, c;
 	srand(time(NULL));
 
-	TTF_Init(); /*Initialise SDL_TTF (police d'écriture) */
-	TTF_Font* police = NULL; /*Pointeur vers police*/
+	TTF_Init();		/*Initialise SDL_TTF (police d'écriture) */
+	TTF_Font *police = NULL;	/*Pointeur vers police */
 	police = TTF_OpenFont("DroidSans.ttf", 10);
-	SDL_Color couleur = {0, 0, 0}; /*Couleur Noire*/
+	SDL_Color couleur = { 0, 0, 0 };	/*Couleur Noire */
 
-	SDL_Surface* nom_sommet = NULL; /*Surface pour l'écriture du sommet */
-	SDL_Surface* point = NULL; /*Surface pour affichage des points*/
-	SDL_Surface* progression = NULL; /*Surface pour la barre de progression*/
+	SDL_Surface *nom_sommet = NULL;	/*Surface pour l'écriture du sommet */
+	SDL_Surface *point = NULL;	/*Surface pour affichage des points */
+	SDL_Surface *progression = NULL;	/*Surface pour la barre de progression */
 
-	SDL_Rect position_sommet; /*Position pour les noms des sommets */
-	SDL_Rect position; /*Position pour la progression*/
-	SDL_Rect position_progression; /*Position pour les sommets */
+	SDL_Rect position_sommet;	/*Position pour les noms des sommets */
+	SDL_Rect position;	/*Position pour la progression */
+	SDL_Rect position_progression;	/*Position pour les sommets */
 
-	point = SDL_CreateRGBSurface(SDL_HWSURFACE, 2, 2, 32, 0, 0, 0, 0);/*Création d'un rectangle 2*2 en couleur 32 bits)*/
-	progression = SDL_CreateRGBSurface(SDL_HWSURFACE, 10, 10, 32, 0, 0, 0, 0);/*Création d'un rectangle 10*10 en couleur 32 bits)*/
+	point = SDL_CreateRGBSurface(SDL_HWSURFACE, 2, 2, 32, 0, 0, 0, 0);	/*Création d'un rectangle 2*2 en couleur 32 bits) */
+	progression = SDL_CreateRGBSurface(SDL_HWSURFACE, 10, 10, 32, 0, 0, 0, 0);	/*Création d'un rectangle 10*10 en couleur 32 bits) */
 
 
-for (i=0;i<len_tab_sommet;i++){
-	
+	for (i = 0; i < len_tab_sommet; i++) {
 
-	/* Position des noms des sommets*/
-	position_sommet.x=sommet[i].coordonnee_x*600.0-5;
-	position_sommet.y=sommet[i].coordonnee_y*600.0;
-	
-	nom_sommet = TTF_RenderText_Blended(police, sommet[i].nom, couleur );/*Nomme les sommets*/
-	SDL_BlitSurface(nom_sommet, NULL, ecran, &position_sommet); /*Affiche les noms*/
+		/* Position des noms des sommets */
+		position_sommet.x = sommet[i].coordonnee_x * 600.0 - 5;
+		position_sommet.y = sommet[i].coordonnee_y * 600.0;
 
-	/*Position des sommets */
-	position.x=sommet[i].coordonnee_x*600.0;
-	position.y=sommet[i].coordonnee_y*600.0;
+		nom_sommet = TTF_RenderText_Blended(police, sommet[i].nom, couleur);	/*Nomme les sommets */
+		SDL_BlitSurface(nom_sommet, NULL, ecran, &position_sommet);	/*Affiche les noms */
 
-	/*Initialises couleur des sommets de manière aléatoire*/
-	a=rand()%(255-0)+0;
-	b=rand()%(255-0)+0;
-	c=rand()%(255-0)+0;
+		/*Position des sommets */
+		position.x = sommet[i].coordonnee_x * 600.0;
+		position.y = sommet[i].coordonnee_y * 600.0;
 
-       	SDL_FillRect(point, NULL, SDL_MapRGB(ecran->format, a, b, c)); /*Place les sommets*/   	
-	SDL_BlitSurface(point, NULL, ecran, &position); /* Collage des sommets sur l'écran*/
-	
-	/*Mise à jour de l'écran*/
-	SDL_Flip(ecran); 
-	
-	/*Idem mais pour la barre de progression*/
-	position_progression.y=650;
-	position_progression.x=i*300/len_tab_sommet+150;
-	SDL_FillRect(progression, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));  
-	SDL_BlitSurface(progression, NULL, ecran, &position_progression); 
-	SDL_Flip(ecran); 
-}	
-	/*On libère la mémoire de la police, des surfaces nom_sommet, point, progression et on stope SDL_TTF*/
+		/*Initialises couleur des sommets de manière aléatoire */
+		a = rand() % (255 - 0) + 0;
+		b = rand() % (255 - 0) + 0;
+		c = rand() % (255 - 0) + 0;
+
+		SDL_FillRect(point, NULL, SDL_MapRGB(ecran->format, a, b, c));	/*Place les sommets */
+		SDL_BlitSurface(point, NULL, ecran, &position);	/* Collage des sommets sur l'écran */
+
+		/*Mise à jour de l'écran */
+		SDL_Flip(ecran);
+
+		/*Idem mais pour la barre de progression */
+		position_progression.y = 650;
+		position_progression.x = i * 300 / len_tab_sommet + 150;
+		SDL_FillRect(progression, NULL,
+			     SDL_MapRGB(ecran->format, 255, 255, 255));
+		SDL_BlitSurface(progression, NULL, ecran,
+				&position_progression);
+		SDL_Flip(ecran);
+	}
+	/*On libère la mémoire de la police, des surfaces nom_sommet, point, progression et on stope SDL_TTF */
 	TTF_CloseFont(police);
-  	SDL_FreeSurface(nom_sommet);
-	SDL_FreeSurface(point); 
-	SDL_FreeSurface(progression); 
+	SDL_FreeSurface(nom_sommet);
+	SDL_FreeSurface(point);
+	SDL_FreeSurface(progression);
 	TTF_Quit();
 	return ecran;
 }
 
 
-SDL_Surface* affiche_cout (SDL_Surface* ecran, float cout)
+SDL_Surface *affiche_cout(SDL_Surface * ecran, float cout)
 {
 	/*Affichage cout : Même principe que pour afficher le nom des sommets :
-On ouvre une police, on initialise couleur, position et on l'affiche.
-Puis on libère la mémoire */
-	
+	   On ouvre une police, on initialise couleur, position et on l'affiche.
+	   Puis on libère la mémoire */
+
 	TTF_Init();
 
-	TTF_Font* police = NULL;
+	TTF_Font *police = NULL;
 	police = TTF_OpenFont("DroidSans.ttf", 25);
-	SDL_Color couleur = {0, 0, 0};
+	SDL_Color couleur = { 0, 0, 0 };
 	SDL_Rect position_cout;
-	SDL_Surface* cout_chemin = NULL;
+	SDL_Surface *cout_chemin = NULL;
 
- 	char buffer_cout [50];
-  	
-  	sprintf (buffer_cout, "%f", cout);/*Convertie le float en chaine de caractère */
+	char buffer_cout[50];
 
-	position_cout.x=470;
-	position_cout.y=620;
-	
-	cout_chemin = TTF_RenderText_Blended(police, "cout :", couleur );
+	sprintf(buffer_cout, "%f", cout);	/*Convertie le float en chaine de caractère */
+
+	position_cout.x = 470;
+	position_cout.y = 620;
+
+	cout_chemin = TTF_RenderText_Blended(police, "cout :", couleur);
 	SDL_BlitSurface(cout_chemin, NULL, ecran, &position_cout);
 
-	position_cout.x=470;
-	position_cout.y=650;
-	
-	cout_chemin = TTF_RenderText_Blended(police, buffer_cout, couleur );
+	position_cout.x = 470;
+	position_cout.y = 650;
+
+	cout_chemin = TTF_RenderText_Blended(police, buffer_cout, couleur);
 	SDL_BlitSurface(cout_chemin, NULL, ecran, &position_cout);
 	SDL_Flip(ecran);
-	
+
 
 	TTF_CloseFont(police);
 	SDL_FreeSurface(cout_chemin);
@@ -159,78 +160,74 @@ Puis on libère la mémoire */
 
 
 
-SDL_Surface* Ligne(SDL_Surface* ecran,float x1,float y1, float x2,float y2)  /*algorithme de Bresenham*/
-{
-	
-	int x,y,ValAbsx,ValAbsy,deplacementx,deplacementy,milieu,i;
-	long couleur = 65536*255 + 256*255 + 255;/*Convertie la couleur en long */
- 	int BytesPixel;
-	char* position;
+SDL_Surface *Ligne(SDL_Surface * ecran, float x1, float y1, float x2,
+		   float y2)
+{				/*algorithme de Bresenham */
 
-	x1=x1*600.0;
-	x2=x2*600.0;
-	y1=y1*600.0;
-	y2=y2*600.0;
+	int x, y, ValAbsx, ValAbsy, deplacementx, deplacementy, milieu, i;
+	long couleur = 65536 * 255 + 256 * 255 + 255;	/*Convertie la couleur en long */
+	int BytesPixel;
+	char *position;
 
-	ValAbsx = abs(x2-x1);
-	ValAbsy = abs(y2-y1);
+	x1 = x1 * 600.0;
+	x2 = x2 * 600.0;
+	y1 = y1 * 600.0;
+	y2 = y2 * 600.0;
 
-	if(x1>x2){
+	ValAbsx = abs(x2 - x1);
+	ValAbsy = abs(y2 - y1);
+
+	if (x1 > x2) {
 		deplacementx = -1;
-	}
-	else{
+	} else {
 		deplacementx = 1;
 	}
-	if(y1>y2){
+	if (y1 > y2) {
 		deplacementy = -1;
-	}
-	else{			
+	} else {
 		deplacementy = 1;
 	}
- 
+
 	x = x1;
 	y = y1;
 
-	if(ValAbsx>ValAbsy) 
-	{
-		milieu = ValAbsx/2;
-		for(i=0;i<ValAbsx;i++)
-		{
+	if (ValAbsx > ValAbsy) {
+		milieu = ValAbsx / 2;
+		for (i = 0; i < ValAbsx; i++) {
 			x = x + deplacementx;
 			milieu = milieu + ValAbsy;
-			if(milieu>ValAbsx)
-			{
+			if (milieu > ValAbsx) {
 				milieu = milieu - ValAbsx;
 				y = milieu + deplacementy;
 			}
 			BytesPixel = ecran->format->BytesPerPixel;
-			position = (unsigned char *)ecran->pixels + y * ecran->pitch + x * BytesPixel;
-			if (BytesPixel==4){
-				*(unsigned long*)position = couleur;
+			position =
+			    (unsigned char *) ecran->pixels +
+			    y * ecran->pitch + x * BytesPixel;
+			if (BytesPixel == 4) {
+				*(unsigned long *) position = couleur;
 			}
 		}
-	}
-	else
-	{
-		milieu = ValAbsy/2;
-		for(i=0;i<ValAbsy;i++)
-		{
-			y = y+ deplacementy;
+	} else {
+		milieu = ValAbsy / 2;
+		for (i = 0; i < ValAbsy; i++) {
+			y = y + deplacementy;
 			milieu += ValAbsx;
- 
-			if(milieu>ValAbsy)
-			{
+
+			if (milieu > ValAbsy) {
 				milieu = milieu - deplacementx;
 				x = x + deplacementx;
 			}
 			BytesPixel = ecran->format->BytesPerPixel;
-			position = (unsigned char *)ecran->pixels + y * ecran->pitch + x * BytesPixel;
-			if (BytesPixel==4){
-				*(unsigned long*)position = couleur;
+			position =
+			    (unsigned char *) ecran->pixels +
+			    y * ecran->pitch + x * BytesPixel;
+			if (BytesPixel == 4) {
+				*(unsigned long *) position = couleur;
 			}
 		}
-			
+
 	}
 
-return ecran;
+	return ecran;
 }
