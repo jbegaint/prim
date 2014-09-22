96f82473e68c84907cc72a34b79854e37ebca23d
jeanbroid <jeanbroid@gmail.com>
2014-09-22 15:48:27 +0200

    oops forgot to push a while ago

--- src/liste.c
+++ src/liste.c
@@ -145,7 +145,7 @@ void afficher_arc(Arc arc)
 	printf("a%d_%d", arc.sommet_depart, arc.sommet_arrive);
 }
 
-int recherche_elt_liste(Liste L, void *elt)
+int recherche_elt_list(Liste L, void *elt)
 {
 	Liste p;
 	for (p = L; !est_vide_liste(p); p = p->suiv) {
